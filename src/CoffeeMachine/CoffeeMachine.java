5a356b0a4771b9ce98411c85357dd9d3ff0d14c9
IzotovaAlina <a.o.izotova@student.khai.edu>
2022-01-31 16:34:30 +0200

    CoffeeMachine 3-rd stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -2,16 +2,36 @@ package CoffeeMachine;
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    public static void main(String[] arg){
-        System.out.println("Write how many cups of coffee you will need:");
+    public static void main(String[] arg) {
         Scanner Scanner = new Scanner(System.in);
-        int input = Scanner.nextInt();
-        int water = 200;
-        int milk = 50;
-        int coffee = 15;
-        System.out.println("For "+ input + " cups of coffee you will need:\n" +
-                + water * input +" ml of water\n" +
-                + milk * input +" ml of milk\n" +
-                + coffee * input +" g of coffee beans");
+
+        System.out.print("Write how many ml of water the coffee machine has: ");
+        int water = Scanner.nextInt();
+
+        System.out.print("Write how many ml of milk the coffee machine has: ");
+        int milk = Scanner.nextInt();
+
+        System.out.print("Write how many grams of coffee beans the coffee machine has: ");
+        int beans = Scanner.nextInt();
+
+        System.out.print("Write how many cups of coffee you will need: ");
+        int cups = Scanner.nextInt();
+
+        int waterCup = water / 200;
+        int milkCup = milk / 50;
+        int beansCup = beans / 15;
+
+        int limit = Math.min(waterCup, Math.min(milkCup, beansCup));
+
+        if (cups > limit) {
+            System.out.println("No, I can make only " + limit + " cup(s) of coffee");
+
+        } else if (cups == limit) {
+            System.out.println("Yes, I can make that amount of coffee");
+
+        } else {
+            int remainder = limit - cups;
+            System.out.println("Yes, I can make that amount of coffee (and even " + remainder + " more than that)");
+        }
     }
 }
