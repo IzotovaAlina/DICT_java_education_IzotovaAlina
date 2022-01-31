aa78beb08cf88de6ddb28e3f8ce5690a94da1ce3
IzotovaAlina <a.o.izotova@student.khai.edu>
2022-01-31 15:55:47 +0200

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,9 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is " + "Alina" + ".");
+        System.out.println("I was created in " + "2022" + ".");
+        System.out.println("Please, remind me your name.");
+    }
+}
